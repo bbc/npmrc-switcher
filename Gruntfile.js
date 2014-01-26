a9181d41fb479c9880f92222cc948138fe1976b7
JakeChampion <jakechampion.jake2@gmail.com>
2014-01-26 21:05:36 +0000

    Add grunt task for testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,24 +3,28 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-        pkg    : grunt.file.readJSON('package.json'),
-        jshint : {
+        pkg      : grunt.file.readJSON('package.json'),
+        jshint   : {
             all     : [ '**/*.js', '!node_modules/**/*'],
             options : {
                 jshintrc : '.jshintrc'
             }
         },
-        jscs   : {
+        jscs     : {
             main    : [ '**/*.js', '!node_modules/**/*'],
             options : {
                 config : '.jscs.json',
             }
+        },
+        nodeunit : {
+            all : ['tests/**/*_test.js']
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks("grunt-jscs-checker");
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-    grunt.registerTask('default', ['jshint', 'jscs']);
+    grunt.registerTask('default', ['jshint', 'jscs', 'nodeunit']);
 
 };
